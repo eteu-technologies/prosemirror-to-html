cf4600fe4ec200a5f38f611068b86546336d3655
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-11-20 17:06:11 +0200

    Use Nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "prosemirror-to-html dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.nodejs
+            pkgs.deno
+          ];
+        };
+      });
+}
